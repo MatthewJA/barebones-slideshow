d6714173eb25111d6ce9cc9cbe9b447f866fc9f9
MatthewJA <matthew.alger@gmail.com>
2014-01-22 02:16:21 +1100

    Working on options

--- barebones-slideshow.php
+++ barebones-slideshow.php
@@ -5,17 +5,80 @@
  * Description: Simple slideshow plugin.
  * Version: 0.0.1
  * Author: MatthewJA
+ * Requires at least: 3.0
+ * Tested up to: 3.8
  * Author URI: http://github.com/MatthewJA
  * License: GPL v2
  */
 
 /* Register functions with WordPress. */
+if (is_admin()) {
+    // Register initialisation.
+    add_action('admin_init', 'barebones_slideshow_init');
+
+    // Register the admin page.
+    add_action('admin_menu', 'barebones_slideshow_menu');
+}
+
+// Register shortcode [barebones-slideshow] to insert slideshows.
 add_shortcode('barebones-slideshow', 'barebones_slideshow_get');
 
-$barebones_slideshow_class = 'barebones-slideshow';
 
+/* Constants. */
+// CSS class of the slideshow.
+$barebones_slideshow_main_class = 'barebones-slideshow';
+
+// CSS class of the images in the slideshow.
+$barebones_slideshow_image_class = 'barebones-slideshow-image';
+
+// CSS class of the caption in the slideshow.
+$barebones_slideshow_caption_class = 'barebones-slideshow-caption';
+
+
+/* Functions. */
+
+/**
+ * Initialise plugin.
+ */
+function barebones_slideshow_init() {
+    // Set up settings.
+    register_setting('barebones-slideshow-slides', 'barebones-slideshow-slides');
+}
+
+/**
+ * Set up the admin panel of the plugin.
+ */
+function barebones_slideshow_menu() {
+    add_options_page('Barebones Slideshow Options', 'Barebones Slideshow', 'manage_options', 'barebones-slideshow', 'barebones_slideshow_options');
+}
+
+/**
+ * Echo the options page HTML.
+ */
+function barebones_slideshow_options() {
+    if (!current_user_can('manage_options')) {
+        wp_die(__( 'You do not have sufficient permissions to access this page.'));
+    }
+
+
+    // Set up page.
+    echo '<div class="wrap"><h2>Barebones Slideshow</h2>';
+
+    // Start options form.
+    echo '<form method="POST" action="options.php">';
+    settings_fields('barebones-slideshow-slides');
+    do_settings_sections('barebones-slideshow');
+
+    // Close page.
+    submit_button();
+    echo '</div>';
+}
+
+/**
+ * Return the slideshow HTML.
+ */
 function barebones_slideshow_get() {
-	return "<div class=\"{$barebones_slideshow_class}\">Hello, world!</div>"
+    return "<div class=\"{$barebones_slideshow_main_class}\">Hello, world!</div>";
 }
 
 /** End of file **/
--- readme.txt
+++ readme.txt
@@ -7,6 +7,6 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 A simple, easily CSS customisable slideshow. Intended to be easy to format for your own layout.
 
 === Description ===
-A simple, easily CSS customisable slideshow. The slideshow is displayed simply as a `div` with class `.barebones-slideshow`. The div contains images, and they have class `.barebones-slideshow-image`. This makes it easy to format.
+A simple, easily CSS customisable slideshow. The slideshow is displayed simply as a `div` with class `.barebones-slideshow`. The div contains images, and they have class `.barebones-slideshow-image`. There can also be captions on the slides, and they will be in a div with class `.barebones-slideshow-caption`. This simple structure makes it easy to format.
 
 More info may be available [at the repository for the plugin](http://github.com/MatthewJA/barebones-slideshow).
